3e643014f9f79911319b2c9807d2d39377e38e29
Wellington de Oliveira <85302795+WellingtonDeOliveira@users.noreply.github.com>
2021-06-24 10:02:51 -0300

    Delete ExercicioResolucao/src/Animal directory

--- ExercicioResolucao/src/Animal/Cavalo.java
+++
@@ -1,8 +0,0 @@
-package Animal;
-public class Cavalo extends Animal{
-
-    public Cavalo(String nome, int idade) {
-        super(nome, idade, "RIIIIII RIIIIIIN", "Correr");
-    }
-    
-}
--- ExercicioResolucao/src/Animal/Preguica.java
+++
@@ -1,8 +0,0 @@
-package Animal;
-public class Preguica extends Animal{
-
-    public Preguica(String nome, int idade) {
-        super(nome, idade, "UUAAAHHHHH", "Escalar arvores");
-    }
-    
-}
