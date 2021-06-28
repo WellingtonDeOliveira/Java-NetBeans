773a0245a18139a655945bb7c162cdcb36597de7
Wellington de Oliveira <85302795+WellingtonDeOliveira@users.noreply.github.com>
2021-06-28 19:14:25 -0300

    excluir

--- ExercicioResolucao/src/Lista5_Q1eQ2/ContaBancaria.java
+++
@@ -1,9 +0,0 @@
-package Lista5_Q1eQ2;
-public interface ContaBancaria {
-    public abstract void criarConta(String senha);
-    public abstract void sacar(double valor);
-    public abstract void depositar(double valor);
-    public abstract void extrado();
-    public abstract void alterarSenha(String senha);
-    public abstract void mostrarEspecial();
-}
